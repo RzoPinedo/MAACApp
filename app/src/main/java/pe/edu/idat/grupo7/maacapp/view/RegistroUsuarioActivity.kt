8e50727ee3d373a611126fb1105bb9ca6ab86aed
Joel1824 <joelramirez9712@gmail.com>
2022-12-14 22:46:35 -0500

    Avance Ramirez

---
+++ app/src/main/java/pe/edu/idat/grupo7/maacapp/view/RegistroUsuarioActivity.kt
@@ -0,0 +1,12 @@
+package pe.edu.idat.grupo7.maacapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pe.edu.idat.grupo7.maacapp.R
+
+class RegistroUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_usuario)
+    }
+}
